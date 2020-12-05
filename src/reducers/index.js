3536c72588f31e8bf902d7cd61d899fdebc6f8b1
Junya <junya010220@gmail.com>
2020-12-05 22:28:00 +0900

    replace type names with constants

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,32 +1,60 @@
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  DELETE_EVENT
+} from '../actions'
+
 // action = {
 //   type: 'CREATE_EVENT',
-//   title: 'オリンピック開催のお知らせ',
-//   body: '2020年に東京でオリンピックを開催します。'
+//   titile: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
 // }
-
+//
+//
+// # before
 // state = []
+//
+// # after
 // state = [
-//   {id:1, title: 'タイトル 1',body:'ボディ1'},
-//   {id:2, title: 'タイトル 2',body:'ボディ2'},
-//   {id:3, title: 'タイトル 3',body:'ボディ3'},
+//   {
+//     id: 1,
+//     titile: '2020東京オリンピックのお知らせ',
+//     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
+//   }
 // ]
-
-
+//
+// # before
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+// ]
+//
+// # after
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+//   {
+//     id: 4,
+//     titile: '2020東京オリンピックのお知らせ',
+//     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
+//   }
+// ]
 const events = (state = [], action) => {
-  switch(action.type){
-    case 'CREATE_EVENT':
-      const event = {title: action.title, body: action.body }
-      const length = state.length 
-      const id =  state.length === 0 ? 1 : state[length - 1].id + 1
+  switch(action.type) {
+    case CREATE_EVENT:
+      const event = { title: action.title, body: action.body }
+      const length = state.length
+      const id = length === 0 ? 1 : state[length - 1].id + 1
       return [...state, { id, ...event }]
-    case 'DELETE_EVENT':
+    case DELETE_EVENT:
       return state.filter(event => event.id !== action.id)
-    case 'DELETE_ALL_EVENTS':
+    case DELETE_ALL_EVENTS:
       return []
-    
     default:
       return state
   }
 }
 
-export default events 
+export default events
